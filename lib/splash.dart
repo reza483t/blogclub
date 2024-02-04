1c3b6d8a9c3f09b1f2bf8311e85d83822138bfa9
SeyyedRezaHejazi <reza.hejazi.555@gmail.com>
2024-02-04 10:51:26 +0330

    In this section, I implemented the splash page

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,18 +2,20 @@ import 'package:blogclub/gen/assets.gen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-   body: Stack(
-    children: [
-      // fit: BoxFit.cover این تیکه کند به این معنا است که شما بیاید و عکسی که در پروژه ایمپورت کردید را فیت با صفحه کاربر بکنید
-      Assets.img.background.splash.image(fit: BoxFit.cover),
-    ],
-   ) ,
-  );
+    return Scaffold(
+      body: Stack(
+        children: [
+          // fit: BoxFit.cover این تیکه کند به این معنا است که شما بیاید و عکسی که در پروژه ایمپورت کردید را فیت با صفحه کاربر بکنید
+          Positioned.fill(
+              child: Assets.img.background.splash.image(fit: BoxFit.cover)),
+              Center(child: Assets.img.icons.logo.svg(width: 100) ,)
+        ],
+      ),
+    );
   }
 }
