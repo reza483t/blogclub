8fabb0ae8d11e594542d85ffe8ceb56039125fc6
SeyyedRezaHejazi <reza.hejazi.555@gmail.com>
2024-02-04 11:02:18 +0330

    Making a timer to stay on one screen for about 2 seconds and then switch to another screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,10 +1,24 @@
 import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context)=> HomeScreen()));
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
